8507b6d57d843c0c5f88c6bdba881b2afcd98dab
root <root@LAPTOP-50N83NMD.>
2024-12-13 13:28:03 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
 import socket
+import select
+
+def serve_client(s):
+    data = s.recv(1024)
+    if data:
+        print(data.decode())
+        if "PING" in data.decode():
+            s.send("+PONG\r\n".encode())
 
 def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept()
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True, backlog=5)
+    fds_to_watch = [server_socket]
     while True:
-        data = conn.recv(1024)
-        if data:
-            commands = data.decode()
-            print(f"Commands: {commands}, len = {len(commands)}")
-            for command in commands:
-                if 'PING' in command:
-                    conn.send("+PONG\r\n".encode())
+        ready_to_read, _, _ = select.select(fds_to_watch, [], [])
+        for s in ready_to_read:
+            if s == server_socket:
+                client_socket, addr = server_socket.accept()
+                fds_to_watch.append(client_socket)
+            else:
+                serve_client(s)
+
+
 if __name__ == "__main__":
     main()
